5f8afdb6db1cc868bbabf2ee12fabdae4146d279
azanniel <leo.azannielttt@gmail.com>
2024-02-15 08:35:22 -0400

    chore: initial commit to orders management api

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+  .get('/', () => {
+    return 'Hello world'
+  })
+
+app.listen(3333, () => {
+  console.log('🔥 HTTP server running')
+})
