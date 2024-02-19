3e1c293ed6f2cd06fe7df7d13eb44ef2b7d468fb
azanniel <leo.azannielttt@gmail.com>
2024-02-19 08:28:32 -0400

    feat: use zod to validate environments variables

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,12 +1,15 @@
 import postgres from 'postgres'
+import chalk from 'chalk'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
+import { env } from '../env'
 
-const connection = postgres('postgresql://docker:docker@localhost:5432/pizzashop', { max: 1 })
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
 await migrate(db, {migrationsFolder: 'drizzle'})
-
 await connection.end()
+
+console.log(chalk.greenBright('Migrations applied successfully!'))
 
 process.exit()
