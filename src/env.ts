0cf79f61de056bb38ad12041d88c5c2c8f442cb5
azanniel <leo.azannielttt@gmail.com>
2024-02-29 13:33:39 -0400

    feat: add jwt and cookie setup

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   API_BASE_URL: z.string().url().min(1),
   AUTH_REDIRECT_URL: z.string().url().min(1),
+  JWT_SECRET_KEY: z.string().min(1),
   DATABASE_URL: z.string().url().min(1),
 })
 
