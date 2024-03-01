10bde9e982daef250e0200834848104c9bf65d4b
azanniel <leo.azannielttt@gmail.com>
2024-03-01 09:12:48 -0400

    feat: auth link route

---
+++ src/http/auth.ts
@@ -0,0 +1,16 @@
+import { Elysia, t } from 'elysia'
+import jwt from '@elysiajs/jwt'
+import cookie from '@elysiajs/cookie'
+import { env } from '../env'
+
+export const auth = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
