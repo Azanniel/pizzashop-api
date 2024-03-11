6af5283934aa5d6ba35eb5d7d3f7df0eb59c5f0f
azanniel <leo.azannielttt@gmail.com>
2024-03-10 22:51:42 -0400

    feat: get profile

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,12 +3,14 @@ import { registerRestaurant } from './routes/register-restaurants'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
+import { getProfile } from './routes/get-profile'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running')
