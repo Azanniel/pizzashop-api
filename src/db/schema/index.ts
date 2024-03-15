22f7dd90a64ae8b4de2a381edbddd23ec0da552e
azanniel <leo.azannielttt@gmail.com>
2024-03-14 22:54:20 -0400

    feat: add product, order and order items

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,3 +1,6 @@
 export * from './users'
 export * from './restaurants'
 export * from './auth-links'
+export * from './products'
+export * from './orders'
+export * from './order-items'
---
+++ src/db/schema/orders.ts
@@ -0,0 +1,28 @@
+import { createId } from '@paralleldrive/cuid2'
+import { integer, pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { users } from '.'
+
+export const orderStatusEnum = pgEnum('order_status', [
+  'pending',
+  'processing',
+  'delivering',
+  'delivered',
+  'canceled',
+])
+
+export const orders = pgTable('orders', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  customerId: text('customer_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+  restaurantId: text('restaurant_id')
+    .notNull()
+    .references(() => users.id, {
+      onDelete: 'cascade',
+    }),
+  status: orderStatusEnum('status').default('pending').notNull(),
+  totalInCents: integer('total_in_cents').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
